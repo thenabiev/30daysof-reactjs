3f6adf051455f90e1a6a3922cf141ffeffc8afe2
thenabiev <sahinnabee@gmail.com>
2023-04-17 15:58:18 +0400

    beta

--- day20-restaurant/src/pages/About.jsx
+++ day20-restaurant/src/pages/About.jsx
@@ -1,10 +1,11 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 import Layout from '../components/Layout/Layout'
 
 const About = () => {
   return (
     <Layout>
-        About
+        <Typography variant='h3' my={3} className='sectionHeader' textAlign={'center'}>About</Typography>
     </Layout>
   )
 }
--- day20-restaurant/src/pages/Home.jsx
+++ day20-restaurant/src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="headerContainer">
         <Typography variant='h2' sx={{color:'#f4f4f4', textShadow:"5px 5px 5px #111"}}>PizzAura</Typography>
         <Typography variant='h5' sx={{color:'#f4f4f4', textShadow:"5px 5px 5px #111"}}>World is round, <br /> but it may be triangle sometimes)</Typography>
-        <Stack direction={'row'} gap={1} my={2}>
+        <Stack direction={{xs:'column', sm:'row'}} gap={1} my={2}>
           <Link to='/contact' >
             <Button color='error' variant='outlined' className='hero-contact' sx={{bgcolor:"#f4f4f4",}}>Contact</Button>
           </Link>
